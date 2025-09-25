5f84fffc44dc0d19d63818b0c2ba649e2cf34031
Nilkanta@33 <mrrabha_nilkanta@protonmail.com>
2025-09-25 14:47:47 +0530

    updated context processor and post list template

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,3 +1,15 @@
 
+# core/context_processors.py
+from django.conf import settings
+
 def site_info(request):
-	return {'brand_name':'Meridian','support_email':'meridian@info.com'}
+    """
+    Adds site-wide info and Supabase/DEBUG settings to the template context.
+    """
+    return {
+        'brand_name': 'Meridian',
+        'support_email': 'meridian@info.com',
+        'debug': settings.DEBUG,
+        'supabase_ref': getattr(settings, 'SUPABASE_PROJECT_REF', ''),
+        'bucket': getattr(settings, 'AWS_STORAGE_BUCKET_NAME', 'blog-images'),
+    }
